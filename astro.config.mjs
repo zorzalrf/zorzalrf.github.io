3f857ec2ff2bfb4d792d83f0686f96f9636272b9
Zorzal RF <zorzalrf@gmail.com>
2026-06-19 07:34:36 -0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://zorzalrf.github.io',
   base: '/',
   trailingSlash: 'always',
+
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
-  }
+  },
+
+  integrations: [sitemap()]
 });
